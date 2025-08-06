3f657e2a620a5edb4d0d00f1a2ef3b8e69ed89db
eazyman92 <132167313+eazyman92@users.noreply.github.com>
2025-08-06 21:52:35 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -8,3 +8,20 @@ else
   echo "The status of the smoke test is failed!"
   exit 1
 fi
+
+echo "=============================================================="
+echo "CHECKING FOR HIGH AND/OR CRITICAL VULNERABILITIES IN THE IMAGE"
+echo "=============================================================="
+
+grep -E "HIGH|CRITICAL" image-report.html > /dev/null
+if [ $? -ne 0 ]; then
+  echo "================================="
+  echo "Docker Image has no vulnerability"
+  echo "================================="
+
+else
+  echo "=============================="
+  echo "Docker Image has vulnerability"
+  echo "=============================="
+fi
+  
