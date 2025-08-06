3a39c104fc45810d5ee2d78a7821413dad50f86e
eazyman92 <132167313+eazyman92@users.noreply.github.com>
2025-08-06 22:03:11 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -9,19 +9,4 @@ else
   exit 1
 fi
 
-echo "=============================================================="
-echo "CHECKING FOR HIGH AND/OR CRITICAL VULNERABILITIES IN THE IMAGE"
-echo "=============================================================="
-
-grep -E "HIGH|CRITICAL" image-report.html > /dev/null
-if [ $? -ne 0 ]; then
-  echo "================================="
-  echo "Docker Image has no vulnerability"
-  echo "================================="
-
-else
-  echo "=============================="
-  echo "Docker Image has vulnerability"
-  echo "=============================="
-fi
   
